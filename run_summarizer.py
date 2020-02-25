c8e5692ec40b8739a96325d92bb8e6356192d0bf
Mauricio Giraldo <hello@mauriciogiraldo.com>
2020-02-25 13:47:42 +1100

    :space_invader: made summarizer multiprocess friendly / run_summarizer now mp

--- run_summarizer.py
+++ run_summarizer.py
@@ -1,6 +1,11 @@
 import time
 import pandas as pd
 import subprocess
+import multiprocessing
+import tqdm
+import argparse
+import os
+from pathlib import Path
 
 """
   Assumes:
@@ -11,17 +16,45 @@ import subprocess
   - the existence of an output folder in ../data/colors_output
 """
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-o', '--output_folder', default="./files/colors_output", type=str, help='output folder (created if not provided)')
+parser.add_argument('-p', '--processes', default=None, type=int, help='how many processes (default: None which lets Python decide)')
+
+args = parser.parse_args()
+
+cpu = args.processes
+output_folder = args.output_folder
+
 count=0
 starttime = time.time()
 
+file_path = output_folder
+# Create the output folder
+if (output_folder is None):
+  print("no folder provided, creating default")
+  base_path = os.path.dirname(os.path.realpath(__file__))
+  files_path = "%s/files" % base_path
+  file_path = "%s/%s" % (files_path, "colors_output")
+  Path(file_path).mkdir(exist_ok=True, parents=True)
+else:
+  Path(file_path).mkdir(exist_ok=True, parents=True)
+
 url_df = pd.read_csv("../data/files-urls.csv", index_col='access_pid')
+
+count = len(url_df.file_key)
+
+print("Summarizing %s files with %s processes" % (count, (multiprocessing.cpu_count() if cpu == None else cpu)))
 
 def summarize_row(access_pid, row):
   file_key = row['file_key'].replace("\"","")
   access_pid = access_pid.replace("\"","")
-  subprocess.run(["python", "summarizer.py", "-w", file_key, "../data/colors_output/%s.json" % access_pid])
+  subprocess.run(["python", "summarizer.py", "-w", file_key, "%s/%s.json" % (file_path, access_pid)])
 
-for access_pid, row in url_df.iterrows():
-  summarize_row(access_pid, row)
+with multiprocessing.Pool(processes=cpu) as pool:
+  for access_pid, row in tqdm.tqdm(url_df.iterrows(), total=count):
+    pool.apply(summarize_row, args=(access_pid, row))
+  pool.close()
+  pool.join()
 
 print('Processed %s files in {} seconds'.format(time.time() - starttime) % count)
--- summarizer.py
+++ summarizer.py
@@ -11,12 +11,14 @@ parser.add_argument('filename', default="summary.json", type=str, help='destinat
 parser.add_argument('-ss', '--summarizer_server', default="http://localhost:4567", type=str, help='summarizer server endpoint that conforms to the https://github.com/mgiraldo/color-summarizer-docker charactoeristics (default: http://localhost:4567)')
 parser.add_argument('-is', '--image_server', default="http://host.docker.internal:8000/", type=str, help='image server endpoint (default: http://host.docker.internal:8000/)')
 parser.add_argument('-w', '--with_image_server', action='store_const', const=True, help='use the image server providing path in image server')
+parser.add_argument('-v', '--verbose', action='store_const', const=True, help='print messages for images processed or not found')
 
 args = parser.parse_args()
 
 summarizer_server = args.summarizer_server
 image_server = args.image_server
 with_image_server = args.with_image_server
+verbose = args.verbose
 image = args.image
 filename = args.filename
 
@@ -28,18 +30,22 @@ def summarize_file():
     url = "%s/?url=%s" % (summarizer_server, image_url)
     if (Path(filename).exists() == False):
         try:
-            print(url)
+            if (verbose):
+                print(url)
             urllib.request.urlretrieve(url, filename)
             return True
-        except:
-            print("Could not retrieve: %s " % url)
+        except BaseException as e:
+            if (verbose):
+                print("Could not retrieve: %s " % url)
+                print("  Error: %s " % e)
             return False
     else:
-        print("File exists: %s" % filename)
+        if (verbose):
+            print("File exists: %s" % filename)
         return False
 
 if __name__ == '__main__':
     starttime = time.time()
     processed = summarize_file()
-    if (processed):
-      print('Processed in {} seconds'.format(time.time() - starttime))
+    if (processed and verbose):
+        print('Processed in {} seconds'.format(time.time() - starttime))
